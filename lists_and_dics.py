470dfef4098d9a882273bc242a4dc1a57f2b4e25
beenjii14 <benjamin_cruz@Outlook.es>
2021-11-16 14:56:02 -0600

    feat: :zap: Concat two dictionaries

--- lists_and_dics.py
+++ lists_and_dics.py
@@ -7,5 +7,14 @@ def main():
     squares_dict = { i: i**3 for i in range(1,101) if i % 3 != 0 }
     print(squares_dict)
 
+    # concat two dictionaries
+    dic1 = {1:"hola", 2:"adios", 3:"hasta luego"}
+    dic2 = {4:"¿Cómo estás?", 5:"¿Te encuentras bien?"}
+    result_concat = {**dic1, **dic2} # option one
+    print(result_concat)
+    # create new dictionary with values of dic1 and dic2
+    result_concat = dic1 | dic2 # option two
+    print(result_concat)
+
 if __name__ == '__main__':
     main()
